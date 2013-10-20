ec069492b480d84519af446263e4e6406c90a949
Mozillazg <opensource.mozillazg@gmail.com>
2013-10-20 22:36:09 +0800

    完成获取单个页面内的所有成员信息

--- src/shanbay.py
+++ src/shanbay.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import requests
 import copy
+import re
+
+from bs4 import BeautifulSoup
+import requests
 
 
 class LoginException(Exception):
@@ -23,6 +26,7 @@ class Shanbay(object):
             self.headers = headers
         self.cookies = None
         self.login(username, password)
+        self.kwargs = dict(cookies=self.cookies, headers=self.headers)
 
     def login(self, username, password, url_login=None):
         """登录扇贝网"""
@@ -59,3 +63,60 @@ class Shanbay(object):
             self.cookies.update(r_login.cookies.get_dict())
         else:
             raise LoginException
+
+    def get_team_url(self):
+        url = requests.get('http://www.shanbay.com/team/', **self.kwargs).url
+        if url == 'http://www.shanbay.com/team/team/':
+            raise Exception(u'你还没有加入任何小组')
+        else:
+            return url
+
+    def get_url_id(self, url):
+        return re.findall(r'(\d+)/?$', url)[0]
+
+    def members(self, team_url=None):
+        if team_url is None:
+            team_url = self.get_team_url()
+        # 小组 id
+        team_id = self.get_url_id(team_url)
+        # 小组管理 - 成员管理 url
+        dismiss_url = 'http://www.shanbay.com/team/show_dismiss/%s/' % team_id
+        return self.single_page_members(dismiss_url)
+
+    def single_page_members(self, dismiss_url):
+        """解析单个页面"""
+        html = requests.get(dismiss_url, **self.kwargs).text
+        soup = BeautifulSoup(html)
+        members_html = soup.find(id='members')
+
+        def get_tag_string(html, class_, tag='td', n=0):
+            """获取单个 tag 的文本数据"""
+            return html.find_all(tag, class_=class_)[n].get_text().strip()
+
+        members = []
+        # 获取成员信息
+        for member_html in members_html.find_all('tr', class_='member'):
+            member_url = member_html.find_all('a', class_='nickname'
+                                              )[0].attrs['href']
+            member = {
+                'id': self.get_url_id(member_url),
+                'nickname': get_tag_string(member_html, 'nickname', 'a'),
+                'role': get_tag_string(member_html, 'role'),
+                'points': get_tag_string(member_html, 'points'),
+                'days': get_tag_string(member_html, 'days'),
+                'rate': get_tag_string(member_html, 'rate'),
+                'checked_yesterday': get_tag_string(member_html, 'checked'),
+                'checked_today': get_tag_string(member_html, 'checked', n=1)
+            }
+            members.append(member)
+        return members
+
+
+if __name__ == '__main__':
+    username = raw_input('username: ')
+    password = raw_input('password: ')
+    shanbay = Shanbay(username, password)
+    members =  shanbay.members()
+    for member in members:
+        print u'{id}, {nickname}, {role}, {role}, {points}, {days}, {rate}, '\
+              '{checked_yesterday}, {checked_today}'.format(**member)
