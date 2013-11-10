529487d4322d71d3c007d4b19239e9e352ea9aa4
Mozillazg <opensource.mozillazg@gmail.com>
2013-11-10 13:27:17 +0800

    解决 py2exe 打包问题

--- setup.py
+++ setup.py
@@ -4,4 +4,14 @@
 from distutils.core import setup
 import py2exe
 
-setup(console=['assistant.py'])
+options = {
+    'py2exe': {
+        'optimize': 2,
+        'compressed': 1,
+        # 'bundle_files': 1,
+        'includes': ['lxml', 'html5lib'],
+        # 'packages': ['lxml', 'html5lib']
+    }
+}
+
+setup(console=['assistant.py'], options=options)
