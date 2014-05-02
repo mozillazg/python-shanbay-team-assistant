877f7e6e1d01a1c4967a66193c138e1e947e5721
Mozillazg <opensource.mozillazg@gmail.com>
2014-05-02 22:22:47 +0800

    增加更新日志

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ options = {
 
 setup(
     name='shanbay-team-assistant',
-    version='0.1.5',
+    version='0.1.6.dev',
     description='shanbay.com team assistant',
     options=options,
     executables=[Executable("assistant.py")]
