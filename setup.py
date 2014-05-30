fd8a1c5a9be3d7e2cf68db58dc7cb3ece69cabf0
Mozillazg <opensource.mozillazg@gmail.com>
2014-05-30 20:07:02 +0800

    update .gitignore

--- setup.py
+++ setup.py
@@ -9,11 +9,7 @@ import assistant
 
 options = {
     'build_exe': {
-        'compressed': True,
-        'optimize': 2,
-        # 'init_script': 'Console',
         'packages': ['lxml', 'bs4', 'requests', 'html5lib', 'argparse', 'shanbay'],
-        # 'packages': ['lxml'],
         'icon': 'shanbay.ico',
         'include_files': [
             'README.md',
